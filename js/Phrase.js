b2f3bac03564cd73f6bb76e42e14b1d033535321
Winnie Kelley <alldayallnight642@icloud.com>
2020-04-01 06:33:58 -0400

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,62 +1,79 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * Game.js */
+ * Phrase.js */
 
- /* 
- * Create Game class methods for start and end games
- * handling interactions
- * getting a random phrase
- * checking for win
- * removing a life from the scoreboard*/
+ /*
+ *create a Phrase class
+ * hanndles the creation of phrases*/
 
- //create a 
- class Game{
-    constructor(){
-        //track number of missed guessess
-        this.missed = 0;
-        //array of phrase objects
-        this.phrases = this.createPhrases();
-        //current phrase object that is being used
-        this.activePhrase = null;
+ //create a Phrase class
+ class Phrase{
+    constructor(phrase){
+        //setting phrase to all lower case letters
+        this.phrase = phrase.toLowerCase();
     }
 
     /**
-    * Creates phrases for use in game
-    * @return {array} An array of phrases that could be used in the game */
-    createPhrases() {
-        let phrases = [
-            new Phrase(`zombies`),
-            new Phrase(`vampires`),
-            new Phrase(`wearwolves`),
-            new Phrase(`demon`),
-            new Phrase(`count dracula`)
-        ]
-        return phrases;
-    };
+    * Display phrase on game board
+    */
+   addPhraseToDisplay() {
+       //display the number of letters in the activePhrase, each letters gets a box
+       //store the phrase div
+       const phraseDiv = document.querySelector('#phrase');
+       const ul = phraseDiv.firstElementChild;
+       console.log(ul);
 
-    /**
-     * Selects random phrase from phrases property
-     * @return {Object} Phrase object chosen to be used
-     */
-    getRandomPhrase(){
-        let randomNumber = Math.floor(Math.random() * this.phrases.length);
-        console.log(randomNumber);
-        let randomPhrase = this.phrases[randomNumber];
-        return randomPhrase;
-    };
+       //create a loop to go through the number of leters in the activePhrase
+       console.log(`active phrase has ${this.phrase.length} letters`);
 
-    /**
-     * Begins game by selecting a random phrase and displaying it to user
-     */
-    startGame() {
-        //hide #overlay div
-        const overlayDiv = document.querySelector('#overlay');
-        overlayDiv.style.display = 'none';
-        
-        //store selected phrase into 'activePhrase'
-        //calls the getRandomPhrase method to select a Phrase object
-        const randomPhrase =  this.getRandomPhrase()
-        randomPhrase.addPhraseToDisplay()
-        this.activePhrase =  randomPhrase;
-    };
+       for(let i = 0; i < this.phrase.length; i++){
+           console.log(`in for loop`);
+           //create li elements
+           const li = document.createElement('li');
+           ul.appendChild(li);
+
+           //if there is a space
+           if(this.phrase.charAt(i) === ' '){
+               li.setAttribute('class', `hide space`);
+           } else{
+               li.setAttribute('class', `hide letter ${this.phrase.charAt(i)}`);
+           }
+       };
+   };
+
+   /**
+    * Checks if passed letter is in phrase
+    * @param (string) letter - Letter to check
+    */
+   
+   checkLetter(letter) {
+       console.log(`in checkLetter()`);
+       console.log(`letter clicked: ${letter.textContent}`);
+       console.log(`phrase is: ${this.phrase}`);
+       const selectedPhrase = this.phrase;
+       console.log(`selected phrase is: ${selectedPhrase}`);
+       let selectedLetter = letter.textContent;
+       console.log(`letteris: ${selectedLetter}`);
+
+       //check to see if letter is included in the phrase
+       console.log(`checkLetter: ${selectedPhrase.includes(selectedLetter)}`);
+       return selectedPhrase.includes(selectedLetter);
+   }
+
+   /**
+    * Displays passed letter on screen after a match is found
+    * @param (string) letter - Letter to display
+    */
+   showMatchedLetter(letter) {
+       const matchLetterElement = document.querySelectorAll('.letter');
+       matchLetterElement.forEach(letterElement =>{
+           console.log(letterElement);
+           console.log(letterElement.textContent);
+           if(letterElement.innerHTML === letter.textContent){
+               letterElement[i].classList.remove('hide');
+               console.log(letterElement);
+               letterElement[i].classList.add('show');
+           }
+       });
+   };
 }
